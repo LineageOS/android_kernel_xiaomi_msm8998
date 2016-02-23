d2f18224f42d4e523c3014e01362e6b080460cbc
Nick Desaulniers <ndesaulniers@google.com>
2016-02-23 14:12:49 -0800

    Silences WLAN, PCIe, and CPU suspend state kernel messages
    Bug: 27134656
    Change-Id: I681ec2171472514489365ca4bfc4ef16d9b344fe

--- kernel/cpu.c
+++ kernel/cpu.c
@@ -640,7 +640,7 @@ int disable_nonboot_cpus(void)
 	 */
 	cpumask_clear(frozen_cpus);
 
-	pr_info("Disabling non-boot CPUs ...\n");
+	pr_debug("Disabling non-boot CPUs ...\n");
 	for_each_online_cpu(cpu) {
 		if (cpu == first_cpu)
 			continue;
@@ -690,7 +690,7 @@ void enable_nonboot_cpus(void)
 	if (cpumask_empty(frozen_cpus))
 		goto out;
 
-	pr_info("Enabling non-boot CPUs ...\n");
+	pr_debug("Enabling non-boot CPUs ...\n");
 
 	arch_enable_nonboot_cpus_begin();
 
