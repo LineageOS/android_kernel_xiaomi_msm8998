2316fbcd7f6c2c44f05c27e443adaa44413f82f5
Paolo Valente <paolo.valente@linaro.org>
2016-05-17 12:40:40 +0200

    BUGFIX: expire bfqq if needed in bfq_bfqq_move
    If bfqq happens to be in service but empty when bfq_bfqq_move is invoked,
    then bfqq is also still marked as busy and bfq_del_bfqq_busy is invoked.
    This triggers the failure of the check that bfq_del_bfqq_busy is not
    invoked on the in-service queue. In particular, this flow of operations
    is imprecise also because it is better to expire and reschedule bfqq
    if in service, so as to make sure that it continues to be served only
    if it makes sense accordi to its new group memebrship.
    
    This commit fixes these problems by just expring bfqq, as a preliminary
    step, if bfqq is in service.

--- block/bfq-iosched.c
+++ block/bfq-iosched.c
@@ -3861,7 +3861,7 @@ static void __bfq_put_async_bfqq(struct bfq_data *bfqd,
 
 	bfq_log(bfqd, "put_async_bfqq: %p", bfqq);
 	if (bfqq) {
-		bfq_bfqq_move(bfqd, bfqq, &bfqq->entity, root_group);
+		bfq_bfqq_move(bfqd, bfqq, root_group);
 		bfq_log_bfqq(bfqd, bfqq, "put_async_bfqq: putting %p, %d",
 			     bfqq, atomic_read(&bfqq->ref));
 		bfq_put_queue(bfqq);
