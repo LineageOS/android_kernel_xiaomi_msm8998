d48c3d0074782df75aebf77e9d4cc8b0097e1059
Paolo Valente <paolo.valente@linaro.org>
2016-06-30 17:03:56 +0200

    BUGFIX: remove incorrect group-related BUG_ON

--- block/bfq-cgroup.c
+++ block/bfq-cgroup.c
@@ -162,7 +162,7 @@ static struct blkcg_gq *bfqg_to_blkg(struct bfq_group *bfqg)
 static struct bfq_group *blkg_to_bfqg(struct blkcg_gq *blkg)
 {
 	struct blkg_policy_data *pd = blkg_to_pd(blkg, &blkcg_policy_bfq);
-	BUG_ON(!pd);
+
 	return pd_to_bfqg(pd);
 }
 
