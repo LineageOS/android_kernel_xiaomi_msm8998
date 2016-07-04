646b03c9a78cb40821fbfc920e938d6535d8cf54
Paolo Valente <paolo.valente@linaro.org>
2016-07-04 19:14:35 +0200

    Drop unnecessary synchronize_rcu

--- block/bfq-iosched.c
+++ block/bfq-iosched.c
@@ -4368,8 +4368,6 @@ static void bfq_exit_queue(struct elevator_queue *e)
 
 	bfq_shutdown_timer_wq(bfqd);
 
-	synchronize_rcu();
-
 	BUG_ON(timer_pending(&bfqd->idle_slice_timer));
 
 #ifdef CONFIG_BFQ_GROUP_IOSCHED
