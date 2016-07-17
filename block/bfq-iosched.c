a702159ae5560d575c3f57a2e52bfdb58e6ab269
Paolo Valente <paolo.valente@linaro.org>
2016-07-17 09:07:41 +0200

    BUGFIX: remove a harmless but meaningless comparison

--- block/bfq-iosched.c
+++ block/bfq-iosched.c
@@ -3604,9 +3604,9 @@ static void bfq_set_next_ioprio_data(struct bfq_queue *bfqq,
 		break;
 	}
 
-	if (bfqq->new_ioprio < 0 || bfqq->new_ioprio >= IOPRIO_BE_NR) {
-		printk(KERN_CRIT "bfq_set_next_ioprio_data: new_ioprio %d\n",
-				 bfqq->new_ioprio);
+	if (bfqq->new_ioprio >= IOPRIO_BE_NR) {
+		pr_crit("bfq_set_next_ioprio_data: new_ioprio %d\n",
+			bfqq->new_ioprio);
 		BUG();
 	}
 
