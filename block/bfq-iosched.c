a7c0e2b2ae81ad3ddcbb449402260544bc471dd9
Paolo Valente <paolo.valente@linaro.org>
2016-08-05 19:52:54 +0200

    BFQ v8r1

--- block/bfq-iosched.c
+++ block/bfq-iosched.c
@@ -4833,7 +4833,7 @@ static struct blkcg_policy blkcg_policy_bfq = {
 static int __init bfq_init(void)
 {
 	int ret;
-	char msg[50] = "BFQ I/O-scheduler: v8";
+	char msg[50] = "BFQ I/O-scheduler: v8r1";
 
 	/*
 	 * Can be 0 on HZ < 1000 setups.
