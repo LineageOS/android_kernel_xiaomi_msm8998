9e97db3f1dc0212f9b2a8d7ce989887987550255
Srikanth Uyyala <suyyala@codeaurora.org>
2017-02-23 16:26:31 +0530

    msm: camera: isp: correct the CX iPeak voting logic
    Do not set turbo clock if voting with TCSR block fails
    
    Change-Id: I66bb6d708134ffa6af1149413d850c80e52d9fe5
    Signed-off-by: Srikanth Uyyala <suyyala@codeaurora.org>

--- drivers/media/platform/msm/camera_v2/isp/msm_isp.h
+++ drivers/media/platform/msm/camera_v2/isp/msm_isp.h
@@ -780,7 +780,6 @@ struct vfe_device {
 	size_t num_norm_clk;
 	bool hvx_clk_state;
 	enum cam_ahb_clk_vote ahb_vote;
-	bool turbo_vote;
 	struct cx_ipeak_client *vfe_cx_ipeak;
 
 	/* Sync variables*/
