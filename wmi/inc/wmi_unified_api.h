eb350edf51680662ae9a6034906144fd18c342af
yeshwanth sriram guntuka <ysriramg@codeaurora.org>
2017-09-06 18:20:04 +0530

    Revert "qcacmn: Configure MAWC NLO parameters to the firmware"
    This reverts commit 8aa9875788a3058f2cc8ed62b1035bcb2f600fdf
    as firmware support is not present.
    
    Change-Id: I79336101d0c177b975c56b7cf65700ef9a358f45
    CRs-Fixed: 2092143

--- wmi/inc/wmi_unified_api.h
+++ wmi/inc/wmi_unified_api.h
@@ -623,9 +623,6 @@ QDF_STATUS wmi_unified_pno_start_cmd(void *wmi_hdl,
 		   uint32_t *gchannel_freq_list);
 #endif
 
-QDF_STATUS wmi_unified_nlo_mawc_cmd(void *wmi_hdl,
-		struct nlo_mawc_params *params);
-
 QDF_STATUS wmi_unified_set_ric_req_cmd(void *wmi_hdl, void *msg,
 			uint8_t is_add_ts);
 
