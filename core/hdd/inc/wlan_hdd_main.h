f2b7b13d384b4c4d2715dc37b26f86bd250cf929
Rajeev Kumar <rajekuma@codeaurora.org>
2017-10-19 19:05:49 -0700

    qcacld-3.0: Add sta_id check before updating sta adapater array
    Add sta_id check before updating sta adapater array in all other
    places.
    
    Change-Id: I1877bf0f7e0d4091877c094e03f9f429cf73dd34
    CRs-Fixed: 2129779

--- core/hdd/inc/wlan_hdd_main.h
+++ core/hdd/inc/wlan_hdd_main.h
@@ -186,7 +186,10 @@
 #define MAX_GENIE_LEN (512)
 #define MIN_GENIE_LEN (2)
 
-#define HDD_MAX_STA_COUNT (WLAN_MAX_STA_COUNT + QDF_MAX_NO_OF_SAP_MODE + 2)
+/* One per STA: 1 for BCMC_STA_ID, 1 for each SAP_SELF_STA_ID,
+ * 1 for WDS_STAID
+ */
+#define HDD_MAX_ADAPTERS (WLAN_MAX_STA_COUNT + QDF_MAX_NO_OF_SAP_MODE + 2)
 
 #define WLAN_CHIP_VERSION   "WCNSS"
 
@@ -1844,10 +1847,7 @@ struct hdd_context_s {
 	qdf_spinlock_t hdd_adapter_lock;
 	qdf_list_t hddAdapters; /* List of adapters */
 
-	/* One per STA: 1 for BCMC_STA_ID, 1 for each SAP_SELF_STA_ID,
-	 * 1 for WDS_STAID
-	 */
-	hdd_adapter_t *sta_to_adapter[HDD_MAX_STA_COUNT];
+	hdd_adapter_t *sta_to_adapter[HDD_MAX_ADAPTERS];
 
 	/** Pointer for firmware image data */
 	const struct firmware *fw;
