a1a8b94df4ad6854863028d0fb5fdeb8583e1ab2
Demon000 <demonsingur@gmail.com>
2017-11-22 20:46:13 +0200

    Revert "msm: camera: isp: Fix stream on timeout in LPM"
    This reverts commit 45521db63391bedf4a988703056a60defb3b66b7.

--- drivers/media/platform/msm/camera_v2/isp/msm_isp_axi_util.c
+++ drivers/media/platform/msm/camera_v2/isp/msm_isp_axi_util.c
@@ -2468,7 +2468,6 @@ int msm_isp_ab_ib_update_lpm_mode(struct vfe_device *vfe_dev, void *arg)
 	int i, rc = 0;
 	uint64_t total_bandwidth = 0;
 	int vfe_idx;
-	uint32_t intf;
 	unsigned long flags;
 	struct msm_vfe_axi_stream *stream_info;
 	struct msm_vfe_dual_lpm_mode *ab_ib_vote = NULL;
@@ -2490,13 +2489,7 @@ int msm_isp_ab_ib_update_lpm_mode(struct vfe_device *vfe_dev, void *arg)
 			stream_info =
 				msm_isp_get_stream_common_data(vfe_dev,
 					ab_ib_vote->stream_src[i]);
-			if (stream_info == NULL)
-				continue;
-			/* loop all stream on current session */
 			spin_lock_irqsave(&stream_info->lock, flags);
-			intf = SRC_TO_INTF(stream_info->stream_src);
-			vfe_dev->axi_data.src_info[intf].lpm =
-				ab_ib_vote->lpm_mode;
 			if (stream_info->state == ACTIVE) {
 				vfe_idx =
 					msm_isp_get_vfe_idx_for_stream(vfe_dev,
@@ -2517,12 +2510,7 @@ int msm_isp_ab_ib_update_lpm_mode(struct vfe_device *vfe_dev, void *arg)
 			stream_info =
 				msm_isp_get_stream_common_data(vfe_dev,
 					ab_ib_vote->stream_src[i]);
-			if (stream_info == NULL)
-				continue;
 			spin_lock_irqsave(&stream_info->lock, flags);
-			intf = SRC_TO_INTF(stream_info->stream_src);
-			vfe_dev->axi_data.src_info[intf].lpm =
-				ab_ib_vote->lpm_mode;
 			if (stream_info->state == PAUSED) {
 				vfe_idx =
 					msm_isp_get_vfe_idx_for_stream(vfe_dev,
@@ -2948,9 +2936,7 @@ static void __msm_isp_stop_axi_streams(struct vfe_device *vfe_dev,
 		 * those state transitions instead of directly forcing stream to
 		 * be INACTIVE
 		 */
-		intf = SRC_TO_INTF(stream_info->stream_src);
-		if ((!vfe_dev->axi_data.src_info[intf].lpm) ||
-			stream_info->state != PAUSED) {
+		if (stream_info->state != PAUSED) {
 			while (stream_info->state != ACTIVE)
 				__msm_isp_axi_stream_update(stream_info,
 						&timestamp);
@@ -2967,12 +2953,10 @@ static void __msm_isp_stop_axi_streams(struct vfe_device *vfe_dev,
 				vfe_dev->hw_info->vfe_ops.axi_ops.
 				clear_wm_irq_mask(vfe_dev, stream_info);
 		}
-		if (stream_info->state == ACTIVE &&
-			!vfe_dev->axi_data.src_info[intf].lpm) {
+		if (stream_info->state == ACTIVE) {
 			init_completion(&stream_info->inactive_comp);
 			stream_info->state = STOP_PENDING;
-		} else if (vfe_dev->axi_data.src_info[intf].lpm ||
-			stream_info->state == PAUSED) {
+		} else if (stream_info->state == PAUSED) {
 			/* don't wait for reg update */
 			stream_info->state = STOP_PENDING;
 			msm_isp_axi_stream_enable_cfg(stream_info);
@@ -3087,7 +3071,6 @@ static int msm_isp_start_axi_stream(struct vfe_device *vfe_dev_ioctl,
 	int k;
 	struct vfe_device *vfe_dev;
 	struct msm_vfe_axi_shared_data *axi_data = &vfe_dev_ioctl->axi_data;
-	uint32_t intf;
 
 	if (stream_cfg_cmd->num_streams > MAX_NUM_STREAM)
 		return -EINVAL;
@@ -3151,7 +3134,7 @@ static int msm_isp_start_axi_stream(struct vfe_device *vfe_dev_ioctl,
 					cfg_wm_irq_mask(vfe_dev, stream_info);
 			}
 		}
-		 intf = SRC_TO_INTF(stream_info->stream_src);
+
 		init_completion(&stream_info->active_comp);
 		stream_info->state = START_PENDING;
 		msm_isp_update_intf_stream_cnt(stream_info, 1);
@@ -3161,11 +3144,6 @@ static int msm_isp_start_axi_stream(struct vfe_device *vfe_dev_ioctl,
 			vfe_dev_ioctl->pdev->id);
 		if (src_state) {
 			src_mask |= (1 << SRC_TO_INTF(stream_info->stream_src));
-			if (vfe_dev_ioctl->axi_data.src_info[intf].lpm) {
-				while (stream_info->state != ACTIVE)
-					__msm_isp_axi_stream_update(
-						stream_info, &timestamp);
-			}
 		} else {
 			for (k = 0; k < stream_info->num_isp; k++) {
 				vfe_dev = stream_info->vfe_dev[k];
