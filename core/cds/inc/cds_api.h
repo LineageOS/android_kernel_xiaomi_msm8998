59032466f54f0b452cb0da38a13b6a4c8bb9ca51
Poddar, Siddarth <siddpodd@codeaurora.org>
2018-01-09 11:24:58 +0530

    qcacld-3.0: track connectivity stats for rx packets
    Track connectivity stats for data rx packets through a
    callback function hdd_tx_rx_collect_connectivity_stats_info().
    
    Change-Id: I045ba694495876273691373a9d0a8995d4aa9c34
    CRs-Fixed: 2161889

--- core/cds/inc/cds_api.h
+++ core/cds/inc/cds_api.h
@@ -507,6 +507,13 @@ QDF_STATUS cds_deregister_dp_cb(void);
  * Return: ARP stats IP to track.
  */
 uint32_t cds_get_arp_stats_gw_ip(void *context);
+/**
+ * cds_get_connectivity_stats_pkt_bitmap() - get pkt-type bitmap
+ * @context: osif dev context
+ *
+ * Return: pkt bitmap to track
+ */
+uint32_t cds_get_connectivity_stats_pkt_bitmap(void *context);
 void cds_incr_arp_stats_tx_tgt_delivered(void);
 void cds_incr_arp_stats_tx_tgt_acked(void);
 
