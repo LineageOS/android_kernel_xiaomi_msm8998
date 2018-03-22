aee9ad11ac0642c7bacd81361eeb4e870fbbf2f8
Demon Singur <demonsingur@gmail.com>
2018-03-22 20:22:47 +0000

    goodix_ta: clean up everything
     * get rid of spi bus support, we only need platform bus
     * move fingerprint detection wakelock time to a define
     * do not dynamically allocate minors, there's only one
       minor needed, 0
     * move wakelock declaration to gf_dev
     * merge enable/disable irq methods
     * remove spi clk control
     * remove nav event support
     * remove unused input events
     * remove unused ioctls
     * remove ioctl type checking, copy_from/to_user already
       handles this
     * remove fasync support
     * move netlink message sending out of notifier, inside
       dedicated worker
     * register platform_driver directly
     * move event handling to a high priority worker
     * do not use conflicting names for struct and instance of it
    
    Change-Id: Iac4951f65d533befe1362a718ce4ce8773d7973c

--- drivers/input/fingerprint/goodix_ta/netlink.c
+++ drivers/input/fingerprint/goodix_ta/netlink.c
@@ -12,10 +12,10 @@
 #include <net/sock.h>
 #include <net/netlink.h>
 
-#define NETLINK_TEST 25
-#define MAX_MSGSIZE 32
-int stringlength(char *s);
-void sendnlmsg(char *message);
+#include "gf_spi.h"
+
+#define MAX_MSG_SIZE 32
+
 static int pid = -1;
 struct sock *nl_sk = NULL;
 
@@ -23,32 +23,31 @@ void sendnlmsg(char *message)
 {
 	struct sk_buff *skb_1;
 	struct nlmsghdr *nlh;
-	int len = NLMSG_SPACE(MAX_MSGSIZE);
-	int slen = 0;
+	int len = NLMSG_SPACE(MAX_MSG_SIZE);
 	int ret = 0;
-	if (!message || !nl_sk || !pid) {
+	int slen;
+
+	if (!message || !nl_sk || !pid)
 		return;
-	}
+
 	skb_1 = alloc_skb(len, GFP_KERNEL);
 	if (!skb_1) {
-		pr_err("alloc_skb error\n");
+		pr_err("%s: failed to allocate network buffer\n", __func__);
 		return;
 	}
-	slen = strlen(message);
-	nlh = nlmsg_put(skb_1, 0, 0, 0, MAX_MSGSIZE, 0);
+
+	nlh = nlmsg_put(skb_1, 0, 0, 0, MAX_MSG_SIZE, 0);
 
 	NETLINK_CB(skb_1).portid = 0;
 	NETLINK_CB(skb_1).dst_group = 0;
 
+	slen = strlen(message);
 	message[slen] = '\0';
-	memcpy(NLMSG_DATA(nlh), message, slen+1);
+	memcpy(NLMSG_DATA(nlh), message, slen + 1);
 
 	ret = netlink_unicast(nl_sk, skb_1, pid, MSG_DONTWAIT);
-	if (!ret) {
-		/*kfree_skb(skb_1);*/
-		pr_err("send msg from kernel to usespace failed ret 0x%x\n", ret);
-	}
-
+	if (!ret)
+		pr_err("%s: failed to send netlink message\n", __func__);
 }
 
 
@@ -57,21 +56,25 @@ void nl_data_ready(struct sk_buff *__skb)
 	struct sk_buff *skb;
 	struct nlmsghdr *nlh;
 	char str[100];
+
 	skb = skb_get (__skb);
-	if (skb->len >= NLMSG_SPACE(0)) {
-		nlh = nlmsg_hdr(skb);
-		memcpy(str, NLMSG_DATA(nlh), sizeof(str));
-		pid = nlh->nlmsg_pid;
-		kfree_skb(skb);
-	}
+
+	if (skb->len < NLMSG_SPACE(0))
+		return;
+
+	nlh = nlmsg_hdr(skb);
+	memcpy(str, NLMSG_DATA(nlh), sizeof(str));
+	pid = nlh->nlmsg_pid;
+
+	kfree_skb(skb);
 }
 
 
-int netlink_init(void)
+void netlink_init(void)
 {
 	struct netlink_kernel_cfg netlink_cfg;
-	memset(&netlink_cfg, 0, sizeof(struct netlink_kernel_cfg));
 
+	memset(&netlink_cfg, 0, sizeof(struct netlink_kernel_cfg));
 	netlink_cfg.groups = 0;
 	netlink_cfg.flags = 0;
 	netlink_cfg.input = nl_data_ready;
@@ -79,20 +82,16 @@ int netlink_init(void)
 
 	nl_sk = netlink_kernel_create(&init_net, NETLINK_TEST,
 			&netlink_cfg);
-
-	if (!nl_sk) {
-		pr_err("create netlink socket error\n");
-		return 1;
-	}
-	return 0;
+	if (!nl_sk)
+		pr_err("%s: failed to create netlink socket\n", __func__);
 }
 
 void netlink_exit(void)
 {
-	if (nl_sk != NULL) {
-		netlink_kernel_release(nl_sk);
-		nl_sk = NULL;
-	}
-	pr_info("self module exited\n");
+	if (!nl_sk)
+		return;
+
+	netlink_kernel_release(nl_sk);
+	nl_sk = NULL;
 }
 
