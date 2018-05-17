ba7fdf5aa50389f976c1cf82152663380c180808
Demon Singur <demonsingur@gmail.com>
2018-05-17 03:28:46 +0000

    goodix_ta: sched boost on irq
    Change-Id: I6df9f054fa402bfab20b0a0214ea729ed31ef24e

--- drivers/input/fingerprint/goodix_ta/gf_spi.h
+++ drivers/input/fingerprint/goodix_ta/gf_spi.h
@@ -46,6 +46,7 @@ struct gf_dev {
 	struct notifier_block notifier;
 	struct workqueue_struct *event_workqueue;
 	struct work_struct event_work;
+	struct delayed_work unboost_work;
 	struct wake_lock fp_wakelock;
 	int event;
 
