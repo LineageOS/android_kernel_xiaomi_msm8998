5eef95daf64d528107139a10ff71f72932e48026
Demon Singur <demonsingur@gmail.com>
2018-05-25 06:53:19 +0000

    goodix_ta: allow filtering home key events
    This makes userspace access more similar to the fpc driver.
    
    Change-Id: I6f5f082edb611cf1c1cf0289816e0f172273ace4

--- drivers/input/fingerprint/goodix_ta/gf_spi.c
+++ drivers/input/fingerprint/goodix_ta/gf_spi.c
@@ -68,6 +68,9 @@ static void gf_irq_config(struct gf_device *gf_dev, bool state) {
 
 static void gf_kernel_key_input(struct gf_device *gf_dev, struct gf_key *gf_key)
 {
+	if (!gf_dev->enable_key_events)
+		return;
+
 	pr_debug("%s: received key, key=%d, value=%d\n",
 			__func__, gf_key->key, gf_key->value);
 
@@ -302,6 +305,43 @@ static struct notifier_block gf_fb_notifier = {
 	.notifier_call = gf_fb_state_callback,
 };
 
+static ssize_t gf_enable_key_events_show(struct device *dev,
+	struct device_attribute *attr, char *buf)
+{
+	struct gf_device *gf_dev = dev_get_drvdata(dev);
+
+	return sprintf(buf, "%d\n", gf_dev->enable_key_events);
+}
+
+static ssize_t gf_enable_key_events_store(struct device *dev,
+	struct device_attribute *attr, const char *buf, size_t count)
+{
+	struct gf_device *gf_dev = dev_get_drvdata(dev);
+	int value;
+	int rc;
+
+	rc = kstrtoint(buf, 10, &value);
+	if (rc)
+		return rc;
+
+	gf_dev->enable_key_events = !!value;
+
+	return count;
+}
+
+static DEVICE_ATTR(enable_key_events, S_IWUSR | S_IRUSR,
+	gf_enable_key_events_show,
+	gf_enable_key_events_store);
+
+static struct attribute *gf_attributes[] = {
+	&dev_attr_enable_key_events.attr,
+	NULL
+};
+
+static const struct attribute_group gf_attribute_group = {
+	.attrs = gf_attributes,
+};
+
 static int gf_probe(struct platform_device *pdev)
 {
 	struct gf_device *gf_dev = &gf;
@@ -312,6 +352,9 @@ static int gf_probe(struct platform_device *pdev)
 	gf_dev->process = NULL;
 	gf_dev->display_on = true;
 	gf_dev->irq_enabled = false;
+	gf_dev->enable_key_events = true;
+
+	platform_set_drvdata(pdev, gf_dev);
 
 	gf_dev->reset_gpio = of_get_named_gpio(pdev->dev.of_node,
 			"fp-gpio-reset", 0);
@@ -371,6 +414,12 @@ static int gf_probe(struct platform_device *pdev)
 		goto error_input_register;
 	}
 
+	rc = sysfs_create_group(&pdev->dev.kobj, &gf_attribute_group);
+	if (rc) {
+		pr_err("%s: failed to create sysfs attributes\n", __func__);
+		goto error_create_attributes;
+	}
+
 	gf_dev->notifier = gf_fb_notifier;
 	fb_register_client(&gf_dev->notifier);
 	gf_dev->event_workqueue = alloc_workqueue("gf-event-wq",
@@ -383,6 +432,8 @@ static int gf_probe(struct platform_device *pdev)
 
 	return 0;
 
+error_create_attributes:
+	input_unregister_device(gf_dev->input);
 error_input_register:
 	input_free_device(gf_dev->input);
 error_input_alloc:
@@ -407,6 +458,8 @@ static int gf_remove(struct platform_device *pdev)
 	destroy_workqueue(gf_dev->event_workqueue);
 
 	fb_unregister_client(&gf_dev->notifier);
+
+	sysfs_remove_group(&pdev->dev.kobj, &gf_attribute_group);
 
 	input_unregister_device(gf_dev->input);
 
--- drivers/input/fingerprint/goodix_ta/gf_spi.h
+++ drivers/input/fingerprint/goodix_ta/gf_spi.h
@@ -58,6 +58,8 @@ struct gf_device {
 	int irq;
 
 	struct task_struct *process;
+
+	bool enable_key_events;
 };
 
 void sendnlmsg(char *message);
