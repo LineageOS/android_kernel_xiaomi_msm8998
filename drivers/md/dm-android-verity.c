b6045e83024b2ff886d15cc0bc2f6a8773763e0b
dianlujitao <dianlujitao@lineageos.org>
2018-10-18 14:42:37 +0800

    ANDROID: dm: Always mount as linear target
     * rootdev is mounted via device mapper with system-as-root layout
     * Lineage builds don't have verity enabled i.e it does even have verity
       metadata appended to the parition. Therefore always add rootdev as
       linear device and map the entire partition.
    
    Change-Id: Idcc7a2e8ad7e709b86bc4b2cec9d4229bc1b9a5f

--- drivers/md/dm-android-verity.c
+++ drivers/md/dm-android-verity.c
@@ -103,9 +103,7 @@ static inline bool default_verity_key_id(void)
 
 static inline bool is_eng(void)
 {
-	static const char typeeng[]  = "eng";
-
-	return !strncmp(buildvariant, typeeng, sizeof(typeeng));
+	return 1;
 }
 
 static inline bool is_userdebug(void)
