19fdaa522d3810783573ed03aa4c3b4e428e19ea
wloot <wlootlxt123@gmail.com>
2019-07-12 02:18:59 +0800

    ASoC: msm8998: fix typo and add check before freeing gpio
    Change-Id: I46093ec856e5c4d8c7663879d8a3f17f62a46506

--- sound/soc/msm/msm8998.c
+++ sound/soc/msm/msm8998.c
@@ -9670,11 +9670,14 @@ static int msm_asoc_machine_remove(struct platform_device *pdev)
 
 	if (pdata->us_p_power)
 		regulator_put(pdata->us_p_power);
-	if (pdata->us_p_power)
+	if (pdata->us_n_power)
 		regulator_put(pdata->us_n_power);
 #endif
 
-	gpio_free(pdata->us_euro_gpio);
+	if (pdata->us_euro_gpio > 0) {
+		gpio_free(pdata->us_euro_gpio);
+		pdata->us_euro_gpio = 0;
+	}
 	i2s_auxpcm_deinit();
 
 	snd_soc_unregister_card(card);
