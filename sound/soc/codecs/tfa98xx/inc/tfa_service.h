664ed0c3ae137a66727f0d9bb6213f76798e6a38
Julian Liu <wlootlxt123@gmail.com>
2019-07-18 00:21:38 +0800

    ASoC: tfa98xx: clean up
    Change-Id: I817b25a61e952f0ecaf4695d4d479a140033ac65

--- sound/soc/codecs/tfa98xx/inc/tfa_service.h
+++ sound/soc/codecs/tfa98xx/inc/tfa_service.h
@@ -20,15 +20,7 @@ extern "C" {
 #include "NXP_I2C.h"
 #endif
 
-/* Linux kernel module defines TFA98XX_GIT_VERSIONS in the linux_driver/Makefile */
-#if !defined(TFA98XX_GIT_VERSIONS)
-#include "versions.h"
-#endif
-#ifdef TFA98XX_GIT_VERSIONS
-  #define TFA98XX_API_REV_STR "v6.6.3"/*TFA98XX_GIT_VERSIONS*/
-#else
-  #define TFA98XX_API_REV_STR "v6.6.3"
-#endif
+#define TFA98XX_API_REV_STR "v6.6.3"
 
 #include "tfa_device.h"
 
